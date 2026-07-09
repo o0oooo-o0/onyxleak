e50bed255e9b84689d15fb5c934fe53b65901327
Scripter.SM <faithsmiley.robloxscripts@gmail.com>
2026-07-09 11:53:11 +0500

    .

--- Dependencies/Library.lua
+++ Dependencies/Library.lua
@@ -2932,18 +2932,6 @@ function Library:CreateWindow(...)
     })
     Library:AddToRegistry(Outer, { BorderColor3 = 'OutlineColor' })
 
-    local OuterRing = Library:Create('Frame', {
-        BackgroundTransparency  = 1;
-        BorderColor3            = Library.OutlineColor;
-        BorderMode              = Enum.BorderMode.Outline;
-        BorderSizePixel         = 1;
-        Position                = UDim2.fromOffset(-4, -4);
-        Size                    = UDim2.new(1, 8, 1, 8);
-        ZIndex                  = 0;
-        Parent                  = Outer;
-    })
-    Library:AddToRegistry(OuterRing, { BorderColor3 = 'OutlineColor' })
-
     Library:MakeDraggable(Outer, (25))
 
     local OuterScale = Library:Create('UIScale', {
