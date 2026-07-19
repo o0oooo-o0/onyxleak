1da809463f531c3f6d54b7cf9859fdf0ee31bd6e
scripter.sm <faithsmiley.robloxscripts@gmail.com>
2026-07-19 23:33:56 +0500

    .

--- Loader.lua
+++ Loader.lua
@@ -8,4 +8,9 @@ _/ __ \\___   /
 
 ]]
 
+if (getgenv().EZ_LOADED) then
+	return;
+end;
+getgenv().EZ_LOADED = true;
+
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/31283a1cc203e3f65710cffc683c3bee.lua"))()
