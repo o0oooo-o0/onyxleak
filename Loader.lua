a991d776c31d9909552ca03043dd518f07dbca66
Scripter.SM <faithsmiley.robloxscripts@gmail.com>
2026-07-19 14:08:31 +0500

    .

---
+++ Loader.lua
@@ -0,0 +1,11 @@
+--[[
+               
+  ____ ________
+_/ __ \\___   /
+\  ___/ /    / 
+ \___  >_____ \
+     \/      \/
+
+]]
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/31283a1cc203e3f65710cffc683c3bee.lua"))()
